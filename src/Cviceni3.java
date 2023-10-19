4c1e97a771e76710d253c66e7ba9b2a5647eaf89
Jovan Gliguroski <gliguroski.jovan@gmail.com>
2023-10-19 16:36:16 +0200

    .

--- src/Cviceni3.java
+++ src/Cviceni3.java
@@ -1,5 +1,7 @@
-
+import java.util.Scanner;
 public class Cviceni3 {
+    static Scanner scanner = new Scanner(System.in);
+    static int prvniCislo, druheCislo;
     static String spojeniSlov(String prvniSlovo, String druheSlovo)
     {
         return prvniSlovo+" "+druheSlovo;
@@ -7,6 +9,17 @@ public class Cviceni3 {
     public static void main(String[] args)
     {
         String finalSlovo = spojeniSlov("Hello","world");
-        System.out.print(finalSlovo);
+        System.out.println(finalSlovo);
+
+        System.out.print("Zadejte prvni cislo: ");
+        try { prvniCislo = scanner.nextInt(); }
+        catch(Exception e){ System.out.println("Zadavat můžeš pouze čísla!");}
+
+        System.out.print("Zadejte druhe cislo: ");
+        try { druheCislo = scanner.nextInt(); }
+        catch(Exception e){ System.out.println("Zadavat můžeš pouze čísla!");}
+
+        int soucet = prvniCislo + druheCislo;
+        System.out.print("Vysledek: "+soucet);
     }
 }
